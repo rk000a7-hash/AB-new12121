3fe17dae62601d768d133c435dcb145c50a49c07
rk000a7-hash <rk000a7@gmail.com>
2026-03-10 21:17:26 +0530

    Update db.py

--- backend/utils/db.py
+++ backend/utils/db.py
@@ -1,5 +1,5 @@
 from pymongo import MongoClient
-from config import Config
+from backend.config import Config
 
 client = MongoClient(Config.MONGO_URI)
 db = client.get_default_database() if client.get_database().name else client['ecommerce_db']
