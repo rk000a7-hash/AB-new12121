45cf27b412c18dbefdcb1acd1092f82fe3933edc
Rajanikanth <rk000a7@gmail.com>
2026-03-11 11:23:03 +0530

    updated project files

--- backend/config.py
+++ backend/config.py
@@ -1,7 +1,7 @@
-import os
 from dotenv import load_dotenv
+import os
 
-load_dotenv()
+load_dotenv(os.path.join(os.path.dirname(__file__), '.env'))
 
 class Config:
     MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017/ecommerce_db")
